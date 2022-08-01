a8145cae46ae89c83937690ce66961a61a7d05cb
shantanudubey95 <shantanu@suggaa.in>
2022-08-01 15:08:32 +0000

    Added 4 basic text inputs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import OTPInputs from "./src/OTPInputs";
+import tw from 'twrnc';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={tw`flex-1 justify-center`}>
         <OTPInputs
           // handleChange={(code) => console.log(code)}
           numberOfInputs={4}
@@ -15,10 +16,4 @@ export default class App extends Component {
   }
 }
 
-const styles= StyleSheet.create({
-  container: {
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
-  }
-})
+
--- src/OTPInputs.tsx
+++ src/OTPInputs.tsx
@@ -1,17 +1,46 @@
-import { View, Text, LayoutChangeEvent, PixelRatio, } from 'react-native'
+import { View, Text, LayoutChangeEvent, PixelRatio, TextInput} from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react';
 import tw from 'twrnc';
 
-type numberOfInputs = 4 | 6;
-
 interface Props {
-    numberOfInputs: number
-    
+    numberOfInputs: 4|6;
   }
 
   export default function OTPInputs({ numberOfInputs }: Props) {
     return (
-        <>
-        </>
+        <View style={tw`w-full flex-row justify-center items-center ` }>
+            <TextInput 
+                style={tw`mx-2.5 h-12 w-12 border-2 rounded-md border-[#04825C]`}
+                keyboardType="numeric"
+                textAlign='center'
+                selectionColor={"#04825C"}
+                // onChangeText={onChangeText}
+                // value={text}
+                />
+            <TextInput 
+                style={tw`mx-2.5 h-12 w-12 border-2 rounded-md border-[#04825C]`}
+                keyboardType="numeric"
+                textAlign='center'
+                selectionColor={"#04825C"}
+                // onChangeText={onChangeText}
+                // value={text}
+                />
+            <TextInput 
+                style={tw`mx-2.5 h-12 w-12 border-2 rounded-md  border-[#04825C]`}
+                keyboardType="numeric"
+                textAlign='center'
+                selectionColor={"#04825C"}
+                // onChangeText={onChangeText}
+                // value={text}
+                />
+            <TextInput 
+                style={tw`mx-2.5 h-12 w-12 border-2 rounded-md border-[#04825C]`}
+                keyboardType="numeric"
+                textAlign='center'
+                selectionColor={"#04825C"}
+                // onChangeText={onChangeText}
+                // value={text}
+                />
+        </View>
     )
   }
